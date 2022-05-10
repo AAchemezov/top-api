6d3f9aec3305ffd0be550baf87e255a49fd0e5fe
a.a.chemezov <a.a.chemezov@gmail.com>
2022-05-10 14:32:46 +0800

    9.41

--- src/auth/user.model.ts
+++ src/auth/user.model.ts
@@ -1,9 +1,9 @@
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses'
 import { prop } from '@typegoose/typegoose'
 
-export interface AuthModel extends Base {}
+export interface UserModel extends Base {}
 
-export class AuthModel extends TimeStamps {
+export class UserModel extends TimeStamps {
 	@prop({ unique: true })
 	email: string
 
